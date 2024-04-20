e5c99e75964aefc5af1dd5f2874cce9ef04379f2
KungfuNaman <naman@namans-air.home>
2024-04-20 16:08:40 +0100

    fixed the path

--- backend/src/main/java/com/example/feedbackloop/service/FeedbackService.java
+++ backend/src/main/java/com/example/feedbackloop/service/FeedbackService.java
@@ -205,7 +205,7 @@ public class FeedbackService {
             }
 
             // Execute Java Pathfinder with the .jpf file
-            Process jpfProcess = Runtime.getRuntime().exec("java -jar ./jpf-core/build/RunJPF.jar " + jpfFilePath);
+            Process jpfProcess = Runtime.getRuntime().exec("java -jar /Users/naman/Documents/Term2/verificationValidation/jpf-core/build/RunJPF.jar " + jpfFilePath);
             int jpfResult = jpfProcess.waitFor();
             if (jpfResult != 0) {
                 String errors = readProcessOutput(jpfProcess.getErrorStream());
